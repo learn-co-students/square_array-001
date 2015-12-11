2351db6512ac93b0196194faa47a266a5ec385bc
jessiegibson <jessie@thirstynyc.com>
2015-12-10 23:46:32 -0700

    Squaring numbers in an array.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |x|
+    new_numbers << x**2
+  end 
+  new_numbers
 end
+
